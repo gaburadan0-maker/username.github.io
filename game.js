c5824c9261db7643c3d30403d4b3abaca1adce62
gaburadan0-maker <gaburadan0@gmail.com>
2026-02-06 20:25:23 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,14 @@
+function startup() {
+  document.getElementById("story").innerText =
+    "Startup-ul a explodat! Ai acum 25.000$. Ce faci mai departe?";
+}
+
+function imobiliare() {
+  document.getElementById("story").innerText =
+    "Piața a scăzut. Ai pierdut bani. Mai ai 6.000$.";
+}
+
+function economisesti() {
+  document.getElementById("story").innerText =
+    "Ai economisit. Crești lent dar sigur.";
+}
